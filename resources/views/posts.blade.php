6476cfd7716d6513a91361ae112549c9c786b12a
arne_van_bavel <arne.vanbavel@protime.eu>
2023-01-10 21:58:42 +0100

    Section 2 of the laravelcast

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,16 +3,16 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">First Post</a></h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, optio voluptatibus unde asperiores quaerat cum. Rem autem minima inventore assumenda quae numquam nemo accusantium repudiandae velit adipisci. Voluptatum, odio sunt.</p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, optio voluptatibus unde asperiores quaerat cum. Rem autem minima inventore assumenda quae numquam nemo accusantium repudiandae velit adipisci. Voluptatum, odio sunt.</p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, optio voluptatibus unde asperiores quaerat cum. Rem autem minima inventore assumenda quae numquam nemo accusantium repudiandae velit adipisci. Voluptatum, odio sunt.</p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <h1>
+                <a href="/posts/<?= $post->slug ?>">
+                    <?= $post->title ?>
+                </a>
+            </h1>
+            <div>
+                <?= $post->excerpt ?>
+            </div>
+        </article>
+    <?php endforeach ?>
 </body>

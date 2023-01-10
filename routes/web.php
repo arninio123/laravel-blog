7ecfbf794c4426401a1e47e336f668a0e6522f8b
arne_van_bavel <arne.vanbavel@protime.eu>
2023-01-10 15:53:54 +0100

    My first lesson, addind simple posts + simple routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{post}', function ($slug) {
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+    
+    if(! file_exists($path)){
+        return redirect('/');
+        #abort(404);
+    }
+
+/*     
+    $post = cache()->remember("posts.{$slug}", 5, function() use ($path){
+        var_dump('Cash for 5 seconds');
+        return file_get_contents($path);
+    }); 
+*/
+
+    $post = cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path));
+
+    return view('post',['post' => $post]);
+
+})->where('post','[A-z_\-]+');

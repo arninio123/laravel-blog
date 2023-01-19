07b44fab9216077c03336dec40dd1a300edb925b
arne_van_bavel <arne.vanbavel@protime.eu>
2023-01-19 14:07:33 +0100

    lesson 4 and 5

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +18,62 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //truncate only needed with db:seed
+        //with migrate:fresh --seed -> the tbales are already dropped
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create([
+            'name' => 'John Doe'
+        ]);
+
+        Post::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        User::factory(3)->create();
+        Post::factory(4)->create();
+        
+        /*
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        $hobby = Category::create([
+            'name' => 'Hobby',
+            'slug' => 'hobby'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My first post',
+            'slug' => 'my-first-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        ]);
+        */
+
+
+        //\App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
